5cffe52cf13ac45a3dc43bd6cbe32f3e96109a34
Nick <605820993@qq.com>
2016-07-08 11:19:08 +0800

    添加修改页面

--- Public/js/detail.js
+++ Public/js/detail.js
@@ -4,4 +4,36 @@ $(document).ready(function() {
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 1 // Creates a dropdown of 15 years to control year
     });
+    var $input = $('.datepicker').pickadate()
+
+// Use the picker object directly.
+    var picker = $input.pickadate('picker')
+    // Using arrays formatted as [YEAR, MONTH, DATE].
+    picker.set('select', joinDate, { format: 'yyyy-mm-dd' })
+
+    console.log(isEditing)
+    if (!isEditing) {
+        // $('#info_form :input').val("")
+    }
+
+    $("#submitButton").click(function (event) {
+        event.preventDefault()
+        var $inputs = $('#info_form :input');
+
+        // not sure if you wanted this, but I thought I'd add it.
+        // get an associative array of just the values.
+        var values = {};
+        $inputs.each(function() {
+            values[this.name] = $(this).val();
+        });
+
+        console.log(values);
+        if (isEditing) {
+
+        } else {
+            $.post(addEndPoint, values, function (data) {
+                console.log(data);
+            })
+        }
+    })
 });
--- StaffManage/Home/Controller/StaffController.class.php
+++ StaffManage/Home/Controller/StaffController.class.php
@@ -2,14 +2,33 @@
 namespace Home\Controller;
 use Think\Controller;
 
-
 class StaffController extends Controller{
     public function index() {
-        $this->detail();
+        $this->edit();
     }
 
-    private function detail() {
-        $this->userInfo = 
+    public function addAction() {
+        $this->ajaxReturn($_REQUEST);
+    }
+
+    public function edit() {
+        $this->userInfo = array("Name" => 'Nick', "Tel" => 13270808162, "EmergCont" => "WTF",
+            "PriSector" => "APP部", "ScdSector" => "iOS组", "Post" => "搬瓦工", "Level" => "level1",
+            "Address" => "NJU", "ManagerID" => "9527", "JoinDate" => '2016-07-04');
+        $this->priSectors = array("department1", "department2", "APP部");
+        $this->scdSectors = array("team1", "team2", "iOS组", "Android组");
+        $this->posts = array("搬瓦工", "岗位1");
+        $this->levels = array("level1", "level2", "level19");
+        $this->isEditing = 'true';
+        $this->display('detail');
+    }
+
+    public function add() {
+        $this->userInfo = array();
+        $this->priSectors = array("department1", "department2", "APP部");
+        $this->scdSectors = array("team1", "team2", "iOS组", "Android组");
+        $this->posts = array("搬瓦工", "岗位1");
+        $this->isEditing = 'false';
         $this->display('detail');
     }
 }
