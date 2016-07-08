f0a9612c9dcfb60042eabb73b18bd39d04eb8c6d
1452712 <nbwjh@126.com>
2016-07-08 12:28:19 +0800

    update

--- StaffManage/Home/Model/Staff/StaffModel.class.php
+++ StaffManage/Home/Model/Staff/StaffModel.class.php
@@ -19,7 +19,7 @@
     Level VARCHAR(40),
     Address VARCHAR(160),
     ManagerID VARCHAR(16),
-    JoinDate DATE DEFAULT CURDATE(),
+    JoinDate DATE ,
     DeletedDate DATE DEFAULT null,
     Authority CHAR(1) DEFAULT '3'
     );
@@ -30,32 +30,34 @@ class StaffModel extends Model {
     private $staff;
 
     //建构和析构函数可改名后在每次操作前后连接
-    function __construct() {
+    public function __construct() {
         //更新数据库连接
-        $staff = new mysqli(/*本地连接*/"localhost", "root", "", "staff");
-        if(!$staff) {
-            alert('连接数据库失败，请检查网络。');
+        //$staff = new mysqli(/*本地连接*/"localhost", "root", "", "staff");
+        $this->staff = mysqli_connect(/*本地连接*/"192.168.11.49", "root", "", "staff");
+        if(!$this->staff) {
+            //alert("连接数据库失败，请检查网络。");
         }
     }
 
     function __destruct() {
-        $this->staff->close();
+        mysqli_close($this->staff);
+        //$this->staff->close();
     }
 
     //返回密码或false（查找失败）
     public function getPassword($ID) {
         if(!$this->staff) {
-            alert('连接数据库失败，请检查网络。');
+            //alert('连接数据库失败，请检查网络。');
             return false;
         }
         /*
-        $stmt = $this->staff->prepare("select Password from staff where ID= ?");
+        $stmt = mysqli_prepare($this->staff,"select Password from staff where ID= ?");
         $stmt->bind_param('s', $ID);
         $stmt->execute();
         */
-        $result = $this->staff->query("select Password from staff where ID='{$ID}'");
-        if($result->num_fields() === 1) {
-            return $result->fetch_array();
+        $result = mysqli_query($this->staff,"select Password from staff where ID='{$ID}'");
+        if(mysqli_num_fields($result) === 1) {
+            return mysqli_fetch_array($result);
         } else {
             return false;
         }
@@ -63,7 +65,7 @@ class StaffModel extends Model {
 
     public function setPassword($ID, $Password) {
         if(!$this->staff) {
-            alert('连接数据库失败，请检查网络。');
+            //alert('连接数据库失败，请检查网络。');
             return false;
         }
         if(strlen($Password) > 16 || strlen($Password) < 6)
@@ -73,8 +75,8 @@ class StaffModel extends Model {
         $stmt->bind_param('ss', $Password, $ID);
         return $stmt->execute();
         */
-            $result = $this->staff->query("update staff set Password = (case when ID='{$ID}' then '{$Password}' end)");
-        if($result->num_fields() === 1) {
+            $result = mysqli_query($this->staff,"update staff set Password = (case when ID='{$ID}' then '{$Password}' end)");
+        if(mysqli_num_fields($result) === 1) {
             return true;
         } else {
             return false;
@@ -84,27 +86,42 @@ class StaffModel extends Model {
     //返回所有用户的ID
     function getAllUsers() {
         if(!$this->staff) {
-            alert('连接数据库失败，请检查网络。');
+            //alert('连接数据库失败，请检查网络。');
             return false;
         }
-        return $this->staff->query("select ID from staff");
+        return mysqli_query($this->staff,"select ID from staff");
+    }
+
+    public function getByID($ID) {
+        if(!$this->staff) {
+            //alert('连接数据库失败，请检查网络。');
+            return false;
+        }
+        if($ID != '') {
+            $result = mysqli_query($this->staff,"select Password from staff where ID='{$ID}'");
+            if(mysqli_num_fields($result) === 1) {
+                return mysqli_fetch_array($result);
+            } else {
+                return false;
+            }
+        }
     }
 
     //传入 一级部门，二级部门，姓名
     //若一级部门空缺，则忽略二级部门
     public function getInfo($Name, $PriSector, $ScdSector, $start, $end) {
         if(!$this->staff) {
-            alert('连接数据库失败，请检查网络。');
+            //alert('连接数据库失败，请检查网络。');
             return false;
         }
         if($PriSector === '') {
-            return $this->staff->query("select * from staff where Name='%{$Name}%' limit {$start}, {$end} - {$start} + 1");
+            return mysqli_query($this->staff,"select * from staff where Name='%{$Name}%' limit {$start}, {$end} - {$start} + 1");
         }
         else if ($ScdSector ==='') {
-            return $this->staff->query("select * from staff where PriSector = '{$PriSector}' and Name='%{$Name}%' limit {$start}, {$end} - {$start} + 1");
+            return mysqli_query($this->staff,"select * from staff where PriSector = '{$PriSector}' and Name='%{$Name}%' limit {$start}, {$end} - {$start} + 1");
         }
         else {
-            return $this->staff->query("select * from staff where PriSector = '{$PriSector}' and ScdSector = '{$ScdSector}' and Name='%{$Name}%' limit limit {$start}, {$end} - {$start} + 1");
+            return mysqli_query($this->staff,"select * from staff where PriSector = '{$PriSector}' and ScdSector = '{$ScdSector}' and Name='%{$Name}%' limit limit {$start}, {$end} - {$start} + 1");
         }
     }
 
@@ -176,13 +193,13 @@ class StaffModel extends Model {
         if(!array_key_exists('ManagerID', $NewStaff)) {
             //加入第一个人
             //修改
-            $result1 = $this->staff->query("select * from staff");
-            if($result1->num_fields() === 0) {
+            $result1 = mysqli_query($this->staff,"select * from staff");
+            if(mysqli_num_fields($result1) === 0) {
                 return true;
             }
             //检查是否存在此直属经理
-            $result2 = $this->staff->query("select * from staff where ID='{$NewStaff['ManagerID']}'");
-            if($result2->num_fields() != 1)
+            $result2 = mysqli_query($this->staff,"select * from staff where ID='{$NewStaff['ManagerID']}'");
+            if(mysqli_num_fields($result2) != 1)
                 return false;
         } else {
             return false;
@@ -192,11 +209,12 @@ class StaffModel extends Model {
     //ID,JoinDate，Authority自动生成
     //返回ID或false（插入失败）
     public function addStaff($NewStaff) {
+        //$this->staff = mysqli_connect(/*本地连接*/"192.168.11.49", "root", "", "staff");
         if(!$this->staff) {
-            alert('连接数据库失败，请检查网络。');
+            //alert('连接数据库失败，请检查网络。');
             return false;
         }
-        if(checkInput($NewStaff) === false) {
+        if($this->checkInput($NewStaff) === false) {
             return false;
         }
         //自动生成ID
@@ -205,12 +223,12 @@ class StaffModel extends Model {
             preg_replace('/\D/s', '', $ID);
             if(strlen($ID) < 16) continue;
             $ID = substr($ID,0,16);
-            $result = $this->staff->query("select * from staff where ID='{$ID}'");
+            $result = mysqli_query($this->staff,"select * from staff where ID='{$ID}'");
             if($result->num_fields() === 1) continue;
         }
-        $result = $this->staff->query("insert into staff(ID,Name,Tel,EmergCont,PriSector,ScdSector,Post,Level,Address,ManagerID) 
+        $result = mysqli_query($this->staff,"insert into staff(ID,Name,Tel,EmergCont,PriSector,ScdSector,Post,Level,Address,ManagerID) 
             values('{$ID}','{$NewStaff['Name']}','{$NewStaff['Tel']}','{$NewStaff['EmergCont']}','{$NewStaff['PriSector']}','{$NewStaff['ScdSector']}','{$NewStaff['Post']}','{$NewStaff['Level']}','{$NewStaff['Address']}','{$NewStaff['ManagerID']}')");
-        if($result->num_fields() === 1) {
+        if(mysqli_num_fields($result) === 1) {
             return $result->fetch_array();
         } else {
             return false;
@@ -219,11 +237,11 @@ class StaffModel extends Model {
 
     public function delStaff($ID) {
         if(!$this->staff) {
-            alert('连接数据库失败，请检查网络。');
+            //alert('连接数据库失败，请检查网络。');
             return false;
         }
-        $result = $this->staff->query("update staff set DeletedDate = CURDATE() where ID='{$ID}'");
-        if($result->num_fields() === 1) {
+        $result = mysqli_query($this->staff,"update staff set DeletedDate = CURDATE() where ID='{$ID}'");
+        if(mysqli_num_fields($result) === 1) {
             return true;
         } else {
             return false;
@@ -234,38 +252,17 @@ class StaffModel extends Model {
     //返回ID或false(如果修改不成功)
     public function update($ID, $NewStaff) {
         if(!$this->staff) {
-            alert('连接数据库失败，请检查网络。');
+            //alert('连接数据库失败，请检查网络。');
             return false;
         }
-        if(checkInput($NewStaff) === false) {
+        if($this->checkInput($NewStaff) === false) {
             return false;
         }
-        $result = $this->staff->query("update staff set Name='{$NewStaff['Name']}',Tel='{$NewStaff['Tel']}',EmergCont='{$NewStaff['EmergCont']}',PriSector='{$NewStaff['PriSector']}',ScdSector='{$NewStaff['ScdSector']}',Post='{$NewStaff['Post']}',Level='{$NewStaff['Level']}',Address='{$NewStaff['Address']}',ManagerID='{$NewStaff['ManagerID']}' where ID ='{$ID}'");
-        if($result->num_fields() === 1) {
+        $result = mysqli_query($this->staff,"update staff set Name='{$NewStaff['Name']}',Tel='{$NewStaff['Tel']}',EmergCont='{$NewStaff['EmergCont']}',PriSector='{$NewStaff['PriSector']}',ScdSector='{$NewStaff['ScdSector']}',Post='{$NewStaff['Post']}',Level='{$NewStaff['Level']}',Address='{$NewStaff['Address']}',ManagerID='{$NewStaff['ManagerID']}' where ID ='{$ID}'");
+        if(mysqli_num_fields($result) === 1) {
             return $result->fetch_array();
         } else {
             return false;
         }
-    }
-
-    public function test() {
-        //for(int $i = 0;$i < 1;$i = $i + 1) {
-
-       // }
-        $Name = 'Alex';$Tel = '13000000000';$EmergCont = 'Jason';$PriSector = 'Development';$Post = 'Engineer';
-        $ScdSector = 'Front-end';$Level = 'Primary';$Address='Shanghai China';$ManagerID = '0000000000000000';
-        $NewStaff = [
-            'Name'=>$Name,
-            'Tel'=>$Tel,
-            'EmergCont'=>$EmergCont,
-            'PriSector'=>$PriSector,
-            'ScdSector'=>$ScdSector,
-            'Post'=>$Post,
-            'Level'=>$Level,
-            'Address'=>$Address,
-            'ManagerID'=>$ManagerID,
-        ];
-        $ID = $this->addStaff($NewStaff);
-        $this->show($ID);
     }
 }
